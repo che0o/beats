69251d7af4789b7b399f0c2b1ce31e41b6f50d01
Gainullin Kirill <gaynullinkh@gmail.com>
2021-07-06 22:24:04 +0300

    gulp hello world

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { series, src, dest, watch, parallel } = require('gulp');
+const del = require('del');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+function clean(cb) {
+    return del('./prod');
+    cb();
+}
+
+
+function css() {
+    return src('dev/css/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest('prod/'))
+}
+
+function html() {
+    return src('dev/index.html')
+        .pipe(dest('prod/'))
+}
+
+
+function watcher() {
+    watch('dev/css/*.scss', css);
+}
+
+exports.clean = clean;
+exports.default = parallel(watcher, series(clean, css, html));
+ 
